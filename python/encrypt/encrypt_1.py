361afaa064aa9452f160a99ab9f15c4c63f930a3
testing.dog <i@testing.dog>
2023-11-13 01:19:51 +0800

    encrypt_1.py

---
+++ python/encrypt/encrypt_1.py
@@ -0,0 +1,69 @@
+import os
+import shutil
+from cryptography.fernet import Fernet
+
+# 生成加密密钥
+def generate_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
+
+# 加载密钥
+def load_key():
+    return open('key.key', 'rb').read()
+
+# 加密文件
+def encrypt_file(file_path, key):
+    fernet = Fernet(key)
+    with open(file_path, 'rb') as file:
+        original_data = file.read()
+    encrypted_data = fernet.encrypt(original_data)
+    with open(file_path, 'wb') as encrypted_file:
+        encrypted_file.write(encrypted_data)
+
+# 解密文件
+def decrypt_file(file_path, key):
+    fernet = Fernet(key)
+    with open(file_path, 'rb') as encrypted_file:
+        encrypted_data = encrypted_file.read()
+    decrypted_data = fernet.decrypt(encrypted_data)
+    with open(file_path, 'wb') as decrypted_file:
+        decrypted_file.write(decrypted_data)
+
+# 加密文件夹
+def encrypt_folder(folder_path, key):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            encrypt_file(file_path, key)
+
+# 解密文件夹
+def decrypt_folder(folder_path, key):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            decrypt_file(file_path, key)
+
+# 主程序
+def main():
+    choice = input("请选择操作（加密/解密）：")
+    path = input("请输入文件或文件夹路径：")
+
+    if choice == "加密":
+        generate_key()
+        key = load_key()
+        if os.path.isfile(path):
+            encrypt_file(path, key)
+        elif os.path.isdir(path):
+            encrypt_folder(path, key)
+    elif choice == "解密":
+        key = load_key()
+        if os.path.isfile(path):
+            decrypt_file(path, key)
+        elif os.path.isdir(path):
+            decrypt_folder(path, key)
+
+    os.remove('key.key')  # 删除密钥文件
+
+if __name__ == '__main__':
+    main()
